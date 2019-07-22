a3b36376456ba2fed5fb164f6dac33d2dfe0eebf
Julian -Home <j.henry.b.orr@gmail.com>
2019-07-22 17:37:10 -0400

    Updates

---
+++ precache-manifest.41f4b81bf686e7a42392c22cb8f55480.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b60d86c152e795f02ee9",
+    "url": "/movieApiSearch/static/js/runtime~main.b60d86c1.js"
+  },
+  {
+    "revision": "c63b3a35bd4f52051884",
+    "url": "/movieApiSearch/static/js/main.c63b3a35.chunk.js"
+  },
+  {
+    "revision": "ac2d4adbf6d6a7b8758f",
+    "url": "/movieApiSearch/static/js/1.ac2d4adb.chunk.js"
+  },
+  {
+    "revision": "c63b3a35bd4f52051884",
+    "url": "/movieApiSearch/static/css/main.39e22f7b.chunk.css"
+  },
+  {
+    "revision": "0743da4d1266b718630edaf9b9014b3b",
+    "url": "/movieApiSearch/index.html"
+  }
+];
